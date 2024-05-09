e8ac50fde24d31bd1bf4f99bc6a48366e7673035
DorothyTom <thyntt10112002@gmail.com>
2024-05-09 11:35:39 +0700

    implement aboutme

--- src/Components/Hr.jsx
+++ src/Components/Hr.jsx
@@ -6,7 +6,7 @@ const Hr = () => {
       <hr style={{
         height: "3px",
         border: "none",
-        background: "linear-gradient(to right, #F8B195, #F67280, #C06C84, #6C5B7B, #355C7D)",
+        background: "linear-gradient(to right, #355C7D, #6C5B7B, #C06C84, #6C5B7B, #355C7D)",
         width: "100%"
       }} />
     </div>
